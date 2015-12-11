e04866b764ed57c835ef8f9f6bd99df48d9994d0
Jackson Gabbard <jg@fb.com[mergetool Kaleidoscope]>
2015-12-11 18:45:27 +0000

    Get a tiny skeleton of a page working

---
+++ client/html/js/components/Chrome.js
@@ -0,0 +1,20 @@
+/** @jsx React.DOM */
+var React = require('react');
+var Chrome = React.createClass({
+  render: function() {
+    return (
+      <div class="appChrome">
+        <div class="header">
+          SOSTools :: Client
+        </div> 
+        <div class="main">
+        </div>
+        <div class="footer">
+          Created at Make Stuff Better Hack #3
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Chrome;
