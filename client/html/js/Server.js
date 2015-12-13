8805976308278b0c9fa83222a39165c92bff857d
Jackson Gabbard <jg@fb.com[mergetool Kaleidoscope]>
2015-12-13 02:16:54 +0000

    Integrate the UserServer into the user main js

--- client/html/js/Server.js
+++ client/html/js/Server.js
@@ -6,6 +6,43 @@ function Server() {
   this._instance = null;
 }
 
+Server.prototype.setupCoreListeners = function() {
+    this._socket.on('connect', function() {
+    SOSEvents.emit('socket_connected');
+    this.authenticate();
+  }.bind(this));
+
+  this._socket.on('message', function(data){
+    SOSEvents.emit('message', data);
+  });
+
+  this._socket.on('disconnect', function(){
+    SOSEvents.emit('socket_disconnected');
+  });
+
+  this._socket.on('error', function() {
+    console.log('error');
+    SOSEvents.emit('socket_error');
+  });
+
+  function _onToken(data) { 
+    console.log('get_token received');
+    console.log(data);
+    var token =  data[0].token;
+    this._token = token;
+    global.localStorage.setItem('authToken', token);
+    SOSEvents.emit('authenticated', token);
+  }
+
+  this._socket.on('get_token', function(data) {
+    _onToken(data);
+  });
+
+  this._socket.on('send_token', function(data) {
+    _onToken(data);
+  });
+};
+
 Server.prototype.setupListeners = function() {
   // hm... ? 
 };
@@ -17,6 +54,7 @@ Server.prototype.getInstance = function() {
 Server.prototype.connect = function() {
   var socket = io('ws://localhost:8887');
   this._socket = socket;
+  this.setupCoreListeners();
   this.setupListeners();
 }
 
@@ -48,7 +86,7 @@ Server.prototype.sendMessage = function(conversationID, messageBody) {
     {
       chatID : chatID,
       message: messageBody,
-      senderToken: Server._token
+      sender_token: this._token
     }
   );
 };
--- client/html/js/rep/RepServer.js
+++ client/html/js/rep/RepServer.js
@@ -11,41 +11,6 @@ RepServer.getInstance = function() {
 
 // TODO: Add validation to each of these, emit error cases
 RepServer.prototype.setupListeners = function() {
-  this._socket.on('connect', function() {
-    SOSEvents.emit('socket_connected');
-    this.authenticate();
-  }.bind(this));
-
-  this._socket.on('message', function(data){
-    SOSEvents.emit('message', data);
-  });
-
-  this._socket.on('disconnect', function(){
-    SOSEvents.emit('socket_disconnected');
-  });
-
-  this._socket.on('error', function() {
-    console.log('error');
-    SOSEvents.emit('socket_error');
-  });
-
-  function _onToken(data) { 
-    console.log('get_token received');
-    console.log(data);
-    var token =  data[0].token;
-    this._token = token;
-    global.localStorage.setItem('authToken', token);
-    SOSEvents.emit('authenticated', token);
-  }
-
-  this._socket.on('get_token', function(data) {
-    _onToken(data);
-  });
-
-  this._socket.on('send_token', function(data) {
-    _onToken(data);
-  });
-
   this._socket.on('get_own_chat_ids', function(data) {
     SOSEvents.emit('owned_chats_loaded', data);
   });
@@ -68,7 +33,7 @@ RepServer.prototype.setupListeners = function() {
 };
 
 RepServer.prototype.getOwnConversations = function() {
-  this._socket.send('get_own_chat_ids', { token: Server._token }); 
+  this._socket.send('get_own_chat_ids', { token: this._token }); 
 };
 
 RepServer.prototype.joinConversation = function(chatID) {
@@ -76,7 +41,7 @@ RepServer.prototype.joinConversation = function(chatID) {
     'join_conversation', 
     {
       chatID: chatID, 
-      token: Server._token
+      token: this._token
     }
   );
 };
@@ -85,7 +50,7 @@ RepServer.prototype.loadConversations = function(loadOnlyEmpty) {
   this._socket.send(
     'get_other_chat_ids',
     { 
-      token : Server._token,
+      token : this._token,
       all : !loadOnlyEmpty
     }
   );
